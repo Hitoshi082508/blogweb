eae06d188f6263df9f5bb880239494ee197539ff
hitoshi082508 <hitoshi082508@gmail.com>
2021-10-31 19:08:13 +0900

    ファーストビューを作成しました。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FirstView } from '../src/components/organisms/FirstView'
+//component
 
 type Props = {
   className?: string
@@ -8,11 +10,21 @@ type Props = {
 const IndexBase: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
-      トップページ
+      <img src="logo.png" />
+      <FirstView />
     </div>
   )
 }
 
 const Index = styled(IndexBase)`
+  background: transparent linear-gradient(180deg, #4594D7 0%, #94D0E9 100%) 0% 0% no-repeat padding-box;
+  img {
+    width: 68px;
+    height: 68px;
+    border-radius: 50%;
+    position: fixed;
+    top: 2%;
+    left: 2%;
+  }
 `
 export default Index
