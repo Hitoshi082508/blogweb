9b45822e66aa57509d0add02695437d387755151
hitoshi082508 <hitoshi082508@gmail.com>
2021-10-31 18:27:31 +0900

    _app.tsxでエラーが起きるので一旦コメントアウト

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import 'styles/reset.css';
-import 'styles/globals.css';
+// import 'styles/reset.css';
+// import 'styles/globals.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import type { AppProps } from 'next/app';
